1f5fad61766797fe47a00a9a7cea1cc0c8c57d5d
Finn Dev <finndev62@gmail.com>
2026-08-02 13:28:40 +0300

    Update fb_clear function to accept color parameter

--- include/framebuffer.h
+++ include/framebuffer.h
@@ -9,7 +9,7 @@ uint32_t fb_get_width(void);
 uint32_t fb_get_height(void);
 void fb_put_pixel(uint32_t x, uint32_t y, uint32_t color);
 void fb_fill_rect(uint32_t x, uint32_t y, uint32_t w, uint32_t, uint32_t h, uint32_t color);
-void fb_clear(void);
+void fb_clear(uint32_t color);
 void     fb_draw_line(int x0, int y0, int x1, int y1, uint32_t color);
 void     fb_draw_circle(int cx, int cy, int r, uint32_t color);
 void     fb_fill_circle(int cx, int cy, int r, uint32_t color);
