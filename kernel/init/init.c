56a013210d29977443c5353ce042106fb12728bc
Finn Dev <finndev62@gmail.com>
2026-08-27 09:00:35 +0300

    Update and rename kernel/kmain.c to kernel/init/init.c

--- kernel/init/init.c
+++ kernel/init/init.c
@@ -1,9 +1,9 @@
-#include "../include/kernel.h"
-#include "../include/scheduler.h"
-#include "../include/font.h"
-#include "../include/framebuffer.h"
-#include "../include/terminus16.h"
-#include "../include/vcore_logo.h"
+#include "include/kernel.h"
+#include "include/scheduler.h"
+#include "include/font.h"
+#include "include/framebuffer.h"
+#include "include/terminus16.h"
+#include "include/vcore_logo.h"
 
 extern void vfs_init();
 extern int rtl8111_init();
